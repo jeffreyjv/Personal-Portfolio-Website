7151905db4fe196b4666c9fa8c71a9afeb178bd3
Jeffrey <jeffreyj.vincent@yahoo.com>
2025-08-10 12:55:50 -0400

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+  return (
+    <div className="min-h-dvh bg-background text--foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+
+    </div>
+  )
+}
