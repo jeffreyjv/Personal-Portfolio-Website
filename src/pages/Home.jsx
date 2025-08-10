f11bc834039cd526a99c1b72158f70efb3e9f046
Jeffrey <jeffreyj.vincent@yahoo.com>
2025-08-10 13:33:19 -0400

    feat: added navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+import { Navbar } from "../components/Navbar.jsx";
+
 export const Home = () => {
   return (
     <div className="min-h-dvh bg-background text--foreground overflow-x-hidden">
       {/* Theme Toggle */}
-      
+      <ThemeToggle />
 
       {/* Navbar */}
-
+      <Navbar />
       {/* Main Content */}
 
       {/* Footer */}
